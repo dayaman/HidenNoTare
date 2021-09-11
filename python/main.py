fb17c54db83e3d62b72d1dd246b8f9ecb612cf89
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-11 22:38:53 +0900

    Upadte: コードをフォーマット

--- python/main.py
+++ python/main.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import random
 from typing import TypedDict, List, Literal, Union
+
 
 class Material:
     def __init__(self, name: str, amount: str, note: str = None):
@@ -10,12 +13,15 @@ class Material:
     def divisions(self, part: int) -> List[Material]:
         return [Material(f"{self.name} (1/{part})", f"{self.amount} (1/{part})", self.note)] * part
 
+
 def sleep(seconds: int) -> None:
     print(f"{seconds} 秒作業")
+
 
 class ZairyoTarinaiError(Exception):
     def __init__(self, material: str):
         super().__init__(f"必要素材 {material} が見つかりませんでした。")
+
 
 class Container:
     def __init__(self, name: str):
@@ -38,6 +44,7 @@ class Container:
         random.shuffle(self.materials)
         print(f"{self.name} を混ぜた")
 
+
 class Pan(Container):
     def heating(self, level: Literal["弱火", "中火", "強火"]) -> None:
         print(f"{level} で {self.name} を加熱した")
@@ -48,16 +55,18 @@ class Pan(Container):
     def cooled(self) -> None:
         print(f"{self.name} から粗熱を取った")
         sleep(300)
-        for material in materials:
+        for material in self.materials:
             material.name = f"粗熱を取った {material.name}"
- 
+
+
 class Mixer(Container):
     def mix(self) -> None:
         super
         print(f"{self.name} でミキサー処理した")
         sleep(20)
-        for material in materials:
+        for material in self.materials:
             material.name = f"ミキサー処理した {material.name}"
+
 
 class Grater:
     def grate(self, material: Material) -> None:
@@ -65,22 +74,26 @@ class Grater:
         sleep(120)
         material.name = f"すりおろされた {material.name}"
 
+
 class MaterialGroups(TypedDict):
     A: List[Material]
     B: List[Material]
     C: List[Material]
 
+
 class MixableWareAlt(TypedDict):
     grater: Grater
     bowl: Container
+
 
 class KitchenWare(TypedDict):
     pan: Pan
     mixable_ware: Union[Mixer, MixableWareAlt]
 
-def cook_hiden_no_tare(material_groups: MaterialGroups,
-                       kitchen_ware: KitchenWare) -> Pan:
+
+def cook_hiden_no_tare(material_groups: MaterialGroups, kitchen_ware: KitchenWare) -> Pan:
     A, B, C = material_groups["A"], material_groups["B"], material_groups["C"]
+    pan = kitchen_ware["pan"]
     print("1. Aをすべて鍋に入れ、弱火で煮込む。焦げ付かないように適度に混ぜる。")
     pan.put(A)
     pan.heating("弱火")
@@ -93,12 +106,12 @@ def cook_hiden_no_tare(material_groups: MaterialGroups,
     mixable_ware = kitchen_ware["mixable_ware"]
     print("")
     if isinstance(mixable_ware, Mixer):
-       print("2-a. Bから酒以外をミキサーにかけ、徐々に酒を足す。")
-       divisioned_sake = sake.divisions(10)
-       mixable_ware.put(B)
-       for _ in range(10):
-           mixable_ware.push(divisioned_sake.pop())
-           mixable_ware.mix()
+        print("2-a. Bから酒以外をミキサーにかけ、徐々に酒を足す。")
+        divisioned_sake = sake.divisions(10)
+        mixable_ware.put(B)
+        for _ in range(10):
+            mixable_ware.push(divisioned_sake.pop())
+            mixable_ware.mix()
     else:
         print("2-b. ミキサーがない場合は、すりおろして混ぜる。")
         bowl, grater = mixable_ware["bowl"], mixable_ware["grater"]
